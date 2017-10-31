94815d17734b4ea83ee6ff40b02410524daaa7df
Mario Hammer <mario.hammer@logicline.de>
2017-10-31 17:49:46 +0100

    some lib and logging shit

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,13 +1,20 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+
+  Logger log = LoggerFactory.getLogger(this.getClass());
 
   @Override
   public void start() {
     vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
+        .requestHandler(req -> {
+          log.debug("request host: " + req.host());
+          req.response().end("Hello Vert.x!");
+        })
         .listen(8080);
   }
 
