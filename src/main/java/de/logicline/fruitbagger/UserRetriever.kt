2f3bd516ffb8176925333bfefab41f56e1285a95
Nick Fode <nickfode@posteo.de>
2018-05-30 11:11:24 +0200

    Refactor user retriever
    forgot to commit this

--- src/main/java/de/logicline/fruitbagger/UserRetriever.kt
+++ src/main/java/de/logicline/fruitbagger/UserRetriever.kt
@@ -6,6 +6,7 @@ import io.vertx.core.json.JsonObject
 import io.vertx.ext.auth.oauth2.AccessToken
 import io.vertx.ext.web.RoutingContext
 import org.mongodb.morphia.Datastore
+import java.util.*
 
 class UserRetriever(private val datastore: Datastore) : Handler<RoutingContext> {
 
@@ -19,41 +20,48 @@ class UserRetriever(private val datastore: Datastore) : Handler<RoutingContext> 
                 ctx.next()
                 return@userInfo
             }
+            // if the user already has registered for this browser session
             if (session.get<Any>("fruitUser") != null) {
                 ctx.next()
                 return@userInfo
             }
-            val userInfo = res.result()
-            user.fetch("https://api.github.com/user/emails") { res2 ->
-                if (res2.failed()) {
-                    ctx.session().destroy()
-                    ctx.fail(res2.cause())
-                    return@fetch
-                }
-                val emails = res2.result().jsonArray()
-                userInfo.put("private_emails", emails)
-
-                val first = emails.stream().filter { email ->
-                    val email1 = email as JsonObject
-                    email1.containsKey("primary") && email1.getBoolean("primary")!!
-                }.findFirst()
-                if (first.isPresent) {
-                    val primaryEmail = first.get() as JsonObject
-                    val email = primaryEmail.getString("email")
-                    val users = datastore.find(FruitUser::class.java).field("email").equalIgnoreCase(email).asList()
-                    var dbuser: FruitUser?
-                    if (users.isEmpty()) {
-                        dbuser = FruitUser(email)
-                        datastore.save(dbuser)
-                    } else
-                        dbuser = users[0]
-                    ctx.session().put("fruitUser", dbuser)
-                    ctx.next()
-                    return@fetch
-                }
-                ctx.fail(403)
-            }
+            registerUser(user, ctx)
         }
 
+    }
+
+    private fun registerUser(user: AccessToken, ctx: RoutingContext) {
+        user.fetch("https://api.github.com/user/emails") { res ->
+            if (res.failed()) {
+                ctx.session().destroy()
+                ctx.fail(res.cause())
+                return@fetch
+            }
+            val emails = res.result().jsonArray()
+            val getPrimaryUserMail = emails.stream().filter { email ->
+                val email1 = email as JsonObject
+                email1.containsKey("primary") && email1.getBoolean("primary")!!
+            }.findFirst()
+
+            if (getPrimaryUserMail.isPresent) {
+                addUserToDb(getPrimaryUserMail, ctx)
+                return@fetch
+            }
+            ctx.fail(403)
+        }
+    }
+
+    private fun addUserToDb(getPrimaryUserMail: Optional<Any>, ctx: RoutingContext) {
+        val primaryEmail = getPrimaryUserMail.get() as JsonObject
+        val email = primaryEmail.getString("email")
+        val users = datastore.find(FruitUser::class.java).field("email").equalIgnoreCase(email).asList()
+        var dbuser: FruitUser?
+        if (users.isEmpty()) {
+            dbuser = FruitUser(email)
+            datastore.save(dbuser)
+        } else
+            dbuser = users[0]
+        ctx.session().put("fruitUser", dbuser)
+        ctx.next()
     }
 }
